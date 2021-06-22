fe8a61345d06835b9997bd9fe1b90f034a173d22
tyaro <tkentyan@hotmail.com>
2021-06-22 23:13:43 +0900

    20210622_0

--- python/DB/GetInitialData.py
+++ python/DB/GetInitialData.py
@@ -6,6 +6,7 @@ from setting import session
 from BinanceTableModel import *
 from const import *
 import time
+from DBUtil import klinesData2db
 
 
 # 通貨ペアマスタテーブルのデータを取得
@@ -30,27 +31,6 @@ def GetSymbolData():
 
     return df
 
-# ローソク足データをDBへ登録
-def klinesData2db(df,tablename):
-    
-    if df.empty:
-        return
-
-    # SQL 重複はアップデート
-    query = 'INSERT INTO %s (symbol,openTime,open,close,high,low,coinVolume,usdtVolume) \
-            VALUES("%s","%s",%s,%s,%s,%s,%s,%s) \
-            ON DUPLICATE KEY UPDATE \
-            open=%s,close=%s,high=%s,low=%s,coinVolume=%s,usdtVolume=%s'
-
-    # ローソク足をDBに登録
-    for _,row in df.iterrows():
-        query1 = query % (tablename,
-           row[SYMBOL_],row[OPEN_TIME_],row[OPEN_],row[CLOSE_],row[HIGH_],row[LOW_],row[VOLUME_],row[QUOTE_ASSET_VOLUME_],
-            row[OPEN_],row[CLOSE_],row[HIGH_],row[LOW_],row[VOLUME_],row[QUOTE_ASSET_VOLUME_]
-            )
-        ENGINE.execute(query1)
-
-    return
 
 # データベース初期セットアップ
 def main():
@@ -73,7 +53,7 @@ def main():
         
         # BTC建てローソク足(1日)データ取得
         print(btcPair,"の1日ローソク足を1500件取得します")
-        dfDaysBTC = BinanceAPI.GetKlinesF(btcPair,1500,'1d')
+        dfDaysBTC = BinanceAPI.GetKlinesS(btcPair,1500,'1d')
         dfDaysBTC[SYMBOL_] = btcPair
         klinesData2db(dfDaysBTC,'BINANCE_KLINES_1DAY_BTC')
 
--- python/DB/GetKlines1Day.py
+++ python/DB/GetKlines1Day.py
@@ -1,7 +1,28 @@
 from setting import session
 from BinanceTableModel import *
 from BinanceAPI import *
+import time
+from DBUtil import klinesData2db
 
 
-def GetKlinesF():
-    
+def main():
+
+    # シンボルリストをDBから取得
+    symbolList = session.query(BINANCE_SYMBOL_MASTER.symbol)
+
+    # 全銘柄
+    for symbol in symbolList:
+        pair = symbol[0]
+        print(pair)
+        # バイナンスから10日分のローソク足を取得
+        df = BinanceAPI.GetKlinesF(pair,10,'1d')
+        # 銘柄情報を追加
+        df[SYMBOL_] = pair
+        # DBへ登録
+        klinesData2db(df,'BINANCE_KLINES_1DAY')
+        # 遅延
+        time.sleep(0.5)
+
+
+if __name__ == "__main__":
+    main()
