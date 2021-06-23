701f706c0eafb83139b7d6e6999c227d538b5a41
tyaro <tkentyan@hotmail.com>
2021-06-24 01:30:06 +0900

    20210623_1 cron処理追加 日足時間足自動取得

---
+++ python/BinanceDB/BinanceTableModel.py
@@ -0,0 +1,86 @@
+import sys
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, Float, DateTime,BIGINT
+from setting import Base
+from setting import ENGINE
+
+#ローソク足モデル
+class BINANCE_KLINES_1DAY_MODEL(Base):
+    __tablename__ = 'BINANCE_KLINES_1DAY'
+    symbol = Column('symbol', String(20),primary_key=True)
+    openTIme = Column('openTime', DateTime,primary_key=True)
+    openPrice = Column('open', Float)
+    closePrice = Column('close', Float)
+    highPrice = Column('high', Float)
+    lowPrice = Column('low', Float)
+    coinVolume = Column('coinVolume', Float)
+    usdtVolume = Column('usdtVolume', Float)
+
+#ローソク足モデル
+class BINANCE_KLINES_1DAY_BTC_MODEL(Base):
+    __tablename__ = 'BINANCE_KLINES_1DAY_BTC'
+    symbol = Column('symbol', String(20),primary_key=True)
+    openTIme = Column('openTime', DateTime,primary_key=True)
+    openPrice = Column('open', Float)
+    closePrice = Column('close', Float)
+    highPrice = Column('high', Float)
+    lowPrice = Column('low', Float)
+    coinVolume = Column('coinVolume', Float)
+    usdtVolume = Column('usdtVolume', Float)
+
+#ローソク足モデル
+class BINANCE_KLINES_4HOUR_MODEL(Base):
+    __tablename__ = 'BINANCE_KLINES_4HOUR'
+    symbol = Column('symbol', String(20),primary_key=True)
+    openTIme = Column('openTime', DateTime,primary_key=True)
+    openPrice = Column('open', Float)
+    closePrice = Column('close', Float)
+    highPrice = Column('high', Float)
+    lowPrice = Column('low', Float)
+    coinVolume = Column('coinVolume', Float)
+    usdtVolume = Column('usdtVolume', Float)
+
+#ローソク足モデル
+class BINANCE_KLINES_1HOUR_MODEL(Base):
+    __tablename__ = 'BINANCE_KLINES_1HOUR'
+    symbol = Column('symbol', String(20),primary_key=True)
+    openTIme = Column('openTime', DateTime,primary_key=True)
+    openPrice = Column('open', Float)
+    closePrice = Column('close', Float)
+    highPrice = Column('high', Float)
+    lowPrice = Column('low', Float)
+    coinVolume = Column('coinVolume', Float)
+    usdtVolume = Column('usdtVolume', Float)
+
+#ローソク足モデル
+class BINANCE_KLINES_1MIN_MODEL(Base):
+    __tablename__ = 'BINANCE_KLINES_1MIN'
+    symbol = Column('symbol', String(20),primary_key=True)
+    openTIme = Column('openTime', DateTime,primary_key=True)
+    openPrice = Column('open', Float)
+    closePrice = Column('close', Float)
+    highPrice = Column('high', Float)
+    lowPrice = Column('low', Float)
+    coinVolume = Column('coinVolume', Float)
+    usdtVolume = Column('usdtVolume', Float)
+
+#TICKER モデル
+class BINANCE_TICKER_INFO_MODEL(Base):
+    __tablename__ = 'BINANCE_TICKER_INFO'
+    symbol = Column('symbol', String(20),primary_key=True)
+    tickerTime = Column('tickerTime', DateTime,primary_key=True)
+    price = Column('price', Float)
+
+#通貨ペアマスタ
+class BINANCE_SYMBOL_MASTER(Base):
+    __tablename__ = 'BINANCE_SYMBOL_MASTER'
+    symbol = Column('symbol', String(20),primary_key = True)
+    openPrice = Column('point', Integer)
+    updatetime = Column('updatetime',DateTime)    
+
+
+def main(args):
+    Base.metadata.create_all(bind=ENGINE)
+
+if __name__ == "__main__":
+    main(sys.argv)
