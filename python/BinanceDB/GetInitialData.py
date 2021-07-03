09f1c73f831db0d9b5ddcb33e7226b5dcabb589f
tyaro <tkentyan@hotmail.com>
2021-07-03 23:46:56 +0900

    2021070300

--- python/BinanceDB/GetInitialData.py
+++ python/BinanceDB/GetInitialData.py
@@ -8,40 +8,16 @@ from const import *
 import time
 from DBUtil import klinesData2db
 
-
-# 通貨ペアマスタテーブルのデータを取得
-def GetSymbolData():
-    # bainanceAPIから先物現在価格(全て)を取得
-    # BUSDや4半期どうちゃらは不要
-    df = BinanceAPI.GetTikcerPriceF()
-    df = df.drop(columns=PRICE_)
-    df = df[~df[SYMBOL_].str.contains('_')]
-    df = df[~df[SYMBOL_].str.contains('BUSD')]
-    df = df.sort_values(SYMBOL_)
-
-    # 既にDB内に登録してあるシンボルは上書きする。なかったらInsert
-    query = """INSERT INTO BINANCE_SYMBOL_MASTER (symbol,point,updatetime) VALUES(%s,%s,%s) ON DUPLICATE KEY UPDATE point=%s ,updatetime=%s"""
-
-    print("先物板通貨ペアを取得してDBにマスタデータとして登録します")
-    print("※BUSDは除外します。")
-    print("※永久のみです")
-    for _,row in df.iterrows():
-        # DFを分解して登録(既にDB内にあった場合は上書きする)
-        ENGINE.execute(query,(row[SYMBOL_],row[POINT_],row[TIME_],row[POINT_],row[TIME_]))
-
-    return df
-
-
 # データベース初期セットアップ
 def main():
 
-    # 通貨ペアマスタテーブルのデータを取得
-    df = GetSymbolData()
-
+    # シンボルリストをDBから取得
+    symbolList = session.query(BINANCE_SYMBOL_MASTER.symbol)
     #'''
     # 全てのペアについてDBに登録する
     print("先物のローソク足を順次取得してデータベースに格納します")
-    for pair in df[SYMBOL_]:
+    for symbol in symbolList:
+        pair = symbol[0]
         
         # BTC建てのシンボル名取得
         btcPair = pair.replace('USDT','BTC')
--- python/setup.sh
+++ python/setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 python /root/opt/BinanceDB/BinanceTableModel.py
+python /root/opt/BinanceDB/GetSymbolData.py
+python /root/opt/BinanceDB/GetSymbolDataSpot.py
 python /root/opt/BinanceDB/GetInitialData.py
 
 /bin/sh /root/opt/setup-cron.sh
